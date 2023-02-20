2957c1f7cb4c82e87b2f5bc4431369169cd550a4
HaiderRafi <haiderrafi.7@gmail.com>
2023-02-20 23:17:48 +0530

    Question Of The Day

---
+++ Minimise_Time_Travel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Minimise_Time_Travel {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int p=sc.nextInt();
+        int q=sc.nextInt();
+        int r=sc.nextInt();
+
+        int a=p+q;
+        int b=p+r;
+        int c=q+r;
+
+
+        if(p+q==q+r){
+            System.out.println(p+q);
+        }else{
+            System.out.println(Math.min(c,(Math.min(a,b))));
+        }
+    }
+}
