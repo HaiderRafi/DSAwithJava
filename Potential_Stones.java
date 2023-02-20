3e3df3efe7f96e81748d11031f398cfd345293c3
HaiderRafi <haiderrafi.7@gmail.com>
2023-02-20 23:14:55 +0530

    Question Of The Day

---
+++ Potential_Stones.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Potential_Stones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int X = sc.nextInt();
+        for (int i = X - K + 1; i < X + K; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
