e5d42ca1166dca8807d8149eb45932aef551d7e1
HaiderRafi <haiderrafi.7@gmail.com>
2023-03-29 13:41:31 +0530

    Question Of The Day

---
+++ Question_Of_The_Day/Fitness.java
@@ -0,0 +1,46 @@
+package Question_Of_The_Day;
+import java.util.*;
+public class Fitness {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n*10);
+    }
+}
+
+
+
+
+/*
+Fitness
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+Everyone wants to be fit nowadays. Similarly, our Swapnil also wants to be fit, so he decided to walk to the office and return home walking. Swapnil's office is X km from his home.
+
+If his office is open 5 days a week, find the number of kilometers Swapnil travels through office trips in a week.
+Input
+Each test case contains a single line consisting of a single integer X.
+
+Constraints:
+1 ≤ X ≤ 10
+Output
+For each test case, output the number of kilometers Swapnil travels through office trips in a week.
+starsASK AI
+BETA
+Simpler Explanation
+Concepts Used
+Example
+Sample Input:
+1
+
+Sample Output:
+10
+
+Sample Input:
+3
+
+Sample Output:
+30
+ */
