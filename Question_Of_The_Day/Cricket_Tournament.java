167e4a378673e00e35e55f9d5aca69dd8e7408ad
HaiderRafi <haiderrafi.7@gmail.com>
2023-05-17 21:20:03 +0530

    Question Of The Day

---
+++ Question_Of_The_Day/Cricket_Tournament.java
@@ -0,0 +1,11 @@
+package Question_Of_The_Day;
+import java.util.*;
+public class Cricket_Tournament {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        System.out.print((n*n)-n);
+
+    }
+}
