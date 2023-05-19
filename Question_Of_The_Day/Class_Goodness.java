6c16ad2f2efd479db3ee8c603562e853e25bed51
HaiderRafi <haiderrafi.7@gmail.com>
2023-05-19 15:40:07 +0530

    Question Of The Day

---
+++ Question_Of_The_Day/Class_Goodness.java
@@ -0,0 +1,12 @@
+package Question_Of_The_Day;
+import java.util.*;
+public class Class_Goodness {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+        System.out.println(x^y);
+    }
+}
